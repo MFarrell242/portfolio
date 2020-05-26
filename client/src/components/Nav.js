0fcb18081dea0b9c973b56704b7283b206572e8f
MFarrell242 <farrell.michael.w@gmail.com>
2020-05-26 14:05:12 -0500

    navbar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -3,7 +3,10 @@ import React from 'react';
 export default function Nav() {
     return(
         <nav className="navBar">
-            <a className="navLink"
+            <h2 className="siteName">Michael Farrell</h2><br/>
+            <a className="navLink" href="/">Home</a>
+            <a className="navLink" href="/Folio">Portfolio</a>
+            <a className="navLink" href="/Contact">Contact</a>
         </nav>
     );
 }
